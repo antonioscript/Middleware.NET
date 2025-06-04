86a761bab81047c814188835c9fd3209cda7de35
Antônio Rocha <antoniojunior159@gmail.com>
2025-06-04 20:58:40 -0300

    Insert Middleware

--- MiddlewareAppication/Middleware.API/Program.cs
+++ MiddlewareAppication/Middleware.API/Program.cs
@@ -9,6 +9,37 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+//Ex.1
+//Middleware Presetation Central (controla todas as solicitações)
+app.Run( async context =>
+{
+    await context.Response.WriteAsync("Hello World");
+});
+
+//Ex.2
+app.Use(async (context, next) =>
+{
+    await context.Response.WriteAsync("Middleware 1: Incoming request");
+    await next();  // Pass control to the next middleware
+    await context.Response.WriteAsync("Middleware 1: Outgoing response");
+});
+
+app.Use(async (context, next) =>
+{
+    await context.Response.WriteAsync("Middleware 2: Incoming request");
+    await next();
+    await context.Response.WriteAsync("Middleware 2: Outgoing response");
+});
+
+app.Run(async (context) =>
+{
+    await context.Response.WriteAsync("Middleware 3: Handling request and terminating pipeline");
+    await context.Response.WriteAsync("Hello, world!");
+});
+
+
+
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
